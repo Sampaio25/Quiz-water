e4b61465be34af6adef79b34a1d7c307b5b822e9
Sampaio25 <marcelinhojogador12@hotmail.com>
2023-03-05 22:42:20 -0300

    commit 1

---
+++ quiz.js
@@ -0,0 +1,120 @@
+let quiz_rules = [
+    "You Have Only 20 Seconds For Each Question.",
+    "Once You Select Any Answer.It Can't Be Undone",
+    "You'll Get Points On The Basic Of Your Correct Answers."
+];
+
+
+let quiz_questions = [
+    // question 1
+    {
+        // question
+        question: "Qual destes produtos têm maior consumo de água na produção?",
+
+        // options
+        options : [
+            "1 xícara de café",
+            "100 gramas de açúcar refinado",
+            "100 gramas de chocolate",
+            "100 gramas de carne bovina",
+        ],
+
+        // correct answer
+        answer: 3,
+    },
+    
+    {
+        // write yout question here
+        question: "Em qual ação você contribui para um maior gasto de água?",
+    
+        options: [
+            "Tomar um banho longo de 45 minutos",
+            "Tomar uma taça de vinho",
+            "Comprar uma blusa de algodão",
+            "Comer um hambúrguer",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Qual é o consumo médio diário de água por pessoa no mundo?",
+    
+        options: [
+            "Cerca de 1000 litros",
+            "Perto dos 100 litros ",
+            "Menos de 50 litros",
+            "Dois litros, já que é o que devemos beber por dia",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 3,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Qual o outro nome para a chuva, que é o retorno da água à Terra?",
+    
+        options: [
+            "Transpiração",
+            "Granizo",
+            "Condensação",
+            "Precipitação",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 4,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "A água é uma substância sem sabor, desse modo, é considerada como uma substância:",
+    
+        options: [
+            "Inodora.",
+            "Insípida",
+            "Incolor",
+            "Transparente",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Which of the following is correct?",
+    
+        options: [
+            "JQuery is a JavaScript Library",
+            "JQuery is a JSON Library",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 1,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "Bootstrap 3 is mobile-first.",
+    
+        options: [
+            "False",
+            "True",
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    {
+        // write yout question here
+        question: "What is a correct syntax to output 'Hello World' in C++?",
+    
+        options: [
+            'System.out.printLn("Hello World")',
+            'cout << "Hello World"',
+            'print("Hello World")',
+        ],
+        // correct option (1,2,3,4 etc)
+        answer: 2,
+    //    selected_answer:null
+    },
+    
+];
